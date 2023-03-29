d5d0be86e5fb24cafbdce66c3588d8d557da3ade
nyk <nykcasseano13@gmail.com>
2023-03-29 16:54:24 -0300

    Versão incial  do código que corresponde ao primeiro dia de trabalho

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Quest1 } from './pages/Quest1';
+import {VF_Quest_1} from './pages/Valores-Fem/Quest_1'
+import {VF_Quest_2} from './pages/Valores-Fem/Quest_2'
+import {VF_Quest_3} from './pages/Valores-Fem/Quest_3'
+import {VM_Quest_1_} from './pages/Valores-Mas/Quest_1'
+import {VM_Quest_2_} from './pages/Valores-Mas/Quest_2'
+import {VM_Quest_3_} from './pages/Valores-Mas/Quest_3'
+
 
 function App() {
   return (
