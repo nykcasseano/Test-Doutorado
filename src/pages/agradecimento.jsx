80b4e9b5a9b1ade8c3f46fc53b93c1f20237f8b7
nyk <nykcasseano13@gmail.com>
2023-04-12 16:15:02 -0300

    Versão incial  do código que corresponde ao primeiro dia de trabalho

--- src/pages/agradecimento.jsx
+++ src/pages/agradecimento.jsx
@@ -6,7 +6,11 @@ import '../App.css'
 export function Agradecimento() {
 
   return (
+    
     <>
+    <div class="container">
+    <div class="Quest">
+      <div class="inputs-container"></div>
             <div>
                         <h1> Agradecemos pela disposição em responder essa pesquisa.
 
@@ -15,7 +19,8 @@ export function Agradecimento() {
             Obrigado! </h1>
             </div>
             
-            
+            </div>
+            </div>
             </>
         
 );
