4c971a3850698ce4d8bebb7ec60ad1badc441807
nyk <nykcasseano13@gmail.com>
2023-06-19 15:55:22 -0300

    último commit

--- src/database/firebase_connection.jsx
+++ src/database/firebase_connection.jsx
@@ -1,20 +1,20 @@
-import { initializeApp } from "firebase/app";
-import 'firebase/database';
+// import { initializeApp } from "firebase/app";
+// import 'firebase/database';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAEMy2rEJiWQJqi3jgf4_WbuSAIXi2n5QE",
-  authDomain: "pesquisa-doutorado-98136.firebaseapp.com",
-  projectId: "pesquisa-doutorado-98136",
-  storageBucket: "pesquisa-doutorado-98136.appspot.com",
-  messagingSenderId: "1011765011183",
-  appId: "1:1011765011183:web:a19ed48c878c447335311a"
-};
-
-
-
-if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-}
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAEMy2rEJiWQJqi3jgf4_WbuSAIXi2n5QE",
+//   authDomain: "pesquisa-doutorado-98136.firebaseapp.com",
+//   projectId: "pesquisa-doutorado-98136",
+//   storageBucket: "pesquisa-doutorado-98136.appspot.com",
+//   messagingSenderId: "1011765011183",
+//   appId: "1:1011765011183:web:a19ed48c878c447335311a"
+// };
 
 
-export default firebase;
+
+// if(!firebase.apps.length){
+//     firebase.initializeApp(firebaseConfig);
+// }
+
+
+// export default firebase;
